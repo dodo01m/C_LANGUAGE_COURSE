83289664daf09af19542c1d11c39dc6a0192e244
kakashi Hotake <manojmondal355@gmail.com>
2023-09-18 09:33:01 +0530

    Done some pattern example

--- Logic Building/Pattern/pattern1.c
+++ Logic Building/Pattern/pattern1.c
@@ -1,7 +1,13 @@
 #include<stdio.h>
 int main() {
-	for(int i = 0; i < 5; i++){
-		printf("*");
+	for(int i =  0; i < 5; i++){
+		for(int i = 0; i < 3; i++){
+			for(int i = 0; i < 5; i++){
+				printf("*");
+
+			}	
+			printf("\n");
+		}
 	}
 	printf("\n");
 	return 0;
